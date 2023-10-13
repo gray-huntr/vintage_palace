b9307f42e285b29b00f5aaddfe86af6e2bfd8706
gray-huntr <lkulecho2001@gmail.com>
2023-10-13 23:47:46 +0300

    Add to cart added

--- app/attendants.py
+++ app/attendants.py
@@ -32,6 +32,7 @@ def attendant_dashboard():
                            database=app.config["DB_NAME"])
     cursor = conn.cursor()
     if request.method == 'POST':
+        session.pop("cart", None)
         art_number = request.form['art_number']
 
         cursor.execute("select * from shoes where art_number = %s ", art_number)
@@ -42,9 +43,41 @@ def attendant_dashboard():
             flash("The art number does not exist try again", "info")
             return redirect("/attendant_dashboard")
     else:
+        session['cart'] = True
         cursor.execute("select * from cart where sold_by = %s", session['attendant'])
         if cursor.rowcount == 0:
             return render_template("attendants/attendant_dashboard.html", msg="Your cart is empty")
         else:
             rows = cursor.fetchall()
-            return render_template("attendants/attendant_dashboard.html", rows=rows)
+            total = 0
+            for row in rows:
+                total = row[5] + total
+            return render_template("attendants/attendant_dashboard.html", rows=rows, total=total)
+
+@app.route("/cart/<action>", methods=['POST','GET'])
+def cart(action):
+    #  connect to database
+    conn = pymysql.connect(host=app.config["DB_HOST"], user=app.config["DB_USERNAME"],
+                           password=app.config["DB_PASSWORD"],
+                           database=app.config["DB_NAME"])
+    cursor = conn.cursor()
+    if request.method == 'POST':
+        if action == 'add':
+            art_number = request.form['art_number']
+            shoe_name = request.form['name']
+            price = int(request.form['price'])
+            quantity = int(request.form['quantity'])
+            total = price * quantity
+
+            cursor.execute("insert into cart(art_number, shoe_name, price,quantity,total, sold_by) "
+                           "values (%s,%s,%s,%s,%s,%s)", (art_number,shoe_name,price,quantity,total,session['attendant']))
+            conn.commit()
+            flash("Product added to cart successfully", "success")
+            return redirect("/attendant_dashboard")
+
+    else:
+        flash("Error occurred try again", "warning")
+        return redirect("/attendant_dashboard")
+
+
+
